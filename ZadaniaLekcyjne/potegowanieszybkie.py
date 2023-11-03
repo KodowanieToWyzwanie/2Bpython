c66b773856423a69ee2de795bee708f8a47ad763
KodowanieToWyzwanie <148467446+KodowanieToWyzwanie@users.noreply.github.com>
2023-11-03 12:25:49 +0100

    Update potegowanieszybkie.py

--- ZadaniaLekcyjne/potegowanieszybkie.py
+++ ZadaniaLekcyjne/potegowanieszybkie.py
@@ -8,3 +8,15 @@ def potega_rek2(a,b):
       return wynik * wynik
     else:
       return a * wynik * wynik
+
+
+
+#####a to potegowanie rekurencyjnie 
+
+def potega_rek(a,b):
+  if b == 0 :
+    return 1
+  else:
+    return a * potega_rek(a, b-1)
+    
+print(potega_rek(3,4))
