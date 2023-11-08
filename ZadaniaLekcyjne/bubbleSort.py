e18eb04bdddb59d81067151a92633b544143c7f6
KodowanieToWyzwanie <148467446+KodowanieToWyzwanie@users.noreply.github.com>
2023-11-08 13:12:38 +0100

    Update bubbleSort.py

--- ZadaniaLekcyjne/bubbleSort.py
+++ ZadaniaLekcyjne/bubbleSort.py
@@ -12,3 +12,24 @@ def bubbleSort():
   return list
 
 bubbleSort()
+
+
+#### wersja ze sprawdzaniem czy lista jest posortowana wcześniej
+lst = [3,6,7,8,8,2,3,1]
+
+# lst.sort()
+# print(lst)
+n = len(lst)
+j = 0
+while j < n-1:
+  swapped = False
+  for i in range(n-1-j):
+    if lst[i]>lst[i+1]:
+      #zamień
+      lst[i],lst[i+1] = lst[i+1],lst[i]
+      swapped = True
+  j += 1
+  print(lst)
+  if not swapped:
+    break
+      
