f774f5fc175e7c6c5f69d7e293bd8cfb871f84d6
KodowanieToWyzwanie <148467446+KodowanieToWyzwanie@users.noreply.github.com>
2023-11-17 12:09:15 +0100

    Create 17_11_2023.py

---
+++ NotatkiLekcja/17_11_2023.py
@@ -0,0 +1,24 @@
+# ** math.pow
+# 2 * 2 * 2  *  
+def potega(a, b):
+  wynik = 1
+  for _ in range(b, 0, -1): #z pętlą for działa szybciej
+    wynik *= a  
+  # while b > 0:
+  #   wynik *= a
+  #   b -= 1
+  return wynik
+
+
+def szybkie_potegowanie_ite(a,b):
+  wynik = 1
+  
+  while b > 0:
+    if  b%2 == 1:
+      wynik *= a
+    a *= a  
+    b = b // 2
+  return wynik
+  
+print(szybkie_potegowanie_ite(2,11))
+#print(potega(2, 11))
