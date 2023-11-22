df5f212096bc26f94af1fc93ae37763387bccd35
KodowanieToWyzwanie <148467446+KodowanieToWyzwanie@users.noreply.github.com>
2023-11-22 10:25:29 +0100

    Create zestaw1liczby.py
    Do zrobienia na lekcji

---
+++ Sprawdziany/zestaw1liczby.py
@@ -0,0 +1,22 @@
+
+
+# Zadanie 1
+# Oblicz sumę dowolnej liczby dwucyfrowej i liczby lustrzanej. Podaj dwa największe dzielniki otrzymanej liczby. Liczbę lustrzaną otrzymujemy z danej liczby przez przestawienie jej cyfr.
+
+
+# Zadanie 2
+# Oblicz sumę dowolnej liczby dwucyfrowej i liczby lustrzanej.  Liczbę lustrzaną otrzymujemy z danej liczby przez przestawienie jej cyfr. sprawdz czy dla kliku wybranych liczb otrzymana suma dzieli się przez sumę cyfr jednej z liczb dwucyfrowej - tj. czy np. 42 +24 dzieli się przez 6
+
+
+
+#Zadanie 3
+# W liczbie trzycyfrowej została zatarta cyfra dziesiątek: 3*4. Jaka jest cyfra dziesiątek jeżeli wiadomo, że liczba ta jest podzielna przez 6 ale nie jest podzielna przez 9?
+
+
+
+#zadanie 4
+#Dwucyfrowa liczba została podzielona przez sumę swoich cyfr. oblicz resztę tego działania? Sprawdź dla kilku przypadków
+
+
+#Zadanie 5 
+# Czy z podanych trzech boków można utworzyć trójkąt?
