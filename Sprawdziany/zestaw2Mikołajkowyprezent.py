b0f363723bb23cf2564f03cd41cf59a9d4442b0d
KodowanieToWyzwanie <148467446+KodowanieToWyzwanie@users.noreply.github.com>
2023-12-15 10:55:50 +0100

    Update zestaw2Mikołajkowyprezent.py

--- Sprawdziany/zestaw2Mikołajkowyprezent.py
+++ Sprawdziany/zestaw2Mikołajkowyprezent.py
@@ -1,15 +1,21 @@
-#zad 1 lampki podłączone są szeregowo. Jeśli, któraś nie działa to nie działa cały lampkowy łańcuch. Sprawdź w tabeli 20 elementowej wypełnionej 0 lub 1 (opcjonalnie False True), reprezentującej łańcuch lampkowy.
+#zad 1 lampki podłączone są szeregowo. Jeśli, któraś nie działa to nie działa cały lampkowy łańcuch. Sprawdź w tabeli 20 elementowej 
+wypełnionej 0 lub 1 (opcjonalnie False True), reprezentującej łańcuch lampkowy.
 #Policz ile jest lampek działających a ile nie - 0/False - nie działają
 #Zamień wszystkie 0 na 1.
 
 
-#zad 2 W tym roku św. Mikołaj związał worek łańcuchem na kod. Aby otworzyć kod trzeba dwa razy podać ten sam kod. Poproś o wpisanie kodu, i ponowne wspisanie i wyświetl "Kod jest poprawny - worek otwarty" jeśli kody się zgadzają lub "próbuj jeszcze raz". Można trzy razy spróbować otworzyć worek - za trzecim razem pojawia się komunikat "Do siego roku"
+#zad 2 W tym roku św. Mikołaj związał worek łańcuchem na kod. Aby otworzyć kod trzeba dwa razy podać ten sam kod. 
+  Poproś o wpisanie kodu, i ponowne wspisanie i wyświetl "Kod jest poprawny - worek otwarty" jeśli kody się zgadzają 
+  lub "próbuj jeszcze raz". Można trzy razy spróbować otworzyć worek - za trzecim razem pojawia się komunikat "Do siego roku"
 
 #zad 3 Narysuj choinkę
 
 
 #zad 4 
-#zad 4 Czy zasługujesz na prezenty  św. Mikołaja? Program pyta się "czy w Twoich oczach zasługujesz na prezent?" Jeśli odpowiedź jest nie (bez względu na wielkość liter) - to program "Do zobaczenia za rok"kończy zadanie,jeśli tak (bez względu na wielkość liter) to program pyta się jakiego rodzaju chcesz dostać prezent - wybierz ten, który chcesz dostać najbardziej: 1. kosmetyki, 2. ubrania, 3. gry 4. książki, 5. elektronikę, 6. inne. Program wypisuje:
+#zad 4 Czy zasługujesz na prezenty  św. Mikołaja? Program pyta się "czy w Twoich oczach zasługujesz na prezent?" 
+Jeśli odpowiedź jest nie (bez względu na wielkość liter) - to program "Do zobaczenia za rok"kończy zadanie,jeśli 
+tak (bez względu na wielkość liter) to program pyta się jakiego rodzaju chcesz dostać prezent - wybierz ten, który 
+chcesz dostać najbardziej: 1. kosmetyki, 2. ubrania, 3. gry 4. książki, 5. elektronikę, 6. inne. Program wypisuje:
 
 # (*) Wynik zapisuje to pliku domikolaja.txt
 
@@ -18,11 +24,13 @@
 
 
 #zad 6
-#Losuj prezent - napisz program, który wylosuje dwa rodzaje prezentów z dostępnej puli. Przykładowa pula: kosmetyki,  ubrania,  gry  książki, elektronikę,  inne. Możesz trzy razy losować, jeśli nie jesteś zadowolony z wyboru
+#Losuj prezent - napisz program, który wylosuje dwa rodzaje prezentów z dostępnej puli. Przykładowa pula: kosmetyki,  
+ubrania,  gry  książki, elektronikę,  inne. Możesz trzy razy losować, jeśli nie jesteś zadowolony z wyboru
 
 
 #zad 7 
-#kalkulator zakupów - najpierw podaj ile chcesz kupić produktów, a potem pytaj się o kolejną cenę produktu, na końcu podaj kwotę zakupów, kwoty można podawać po przecinku
+#kalkulator zakupów - najpierw podaj ile chcesz kupić produktów, a potem pytaj się o kolejną cenę produktu, na końcu 
+podaj kwotę zakupów, kwoty można podawać po przecinku
 #wersja 2 bez limitu liczby produktów - pytaj się za każdym razem czy coś jeszcze, na końcu podaj wynik
 
 
