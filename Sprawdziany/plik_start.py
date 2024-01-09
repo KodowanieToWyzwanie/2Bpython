bb5e86788b70fe7a603f45151caef8a0a7c3cb7c
KodowanieToWyzwanie <148467446+KodowanieToWyzwanie@users.noreply.github.com>
2024-01-09 21:38:03 +0100

    Create plik_start.py

---
+++ Sprawdziany/plik_start.py
@@ -0,0 +1,7 @@
+# Grupa 1
+# Do tabeli wpisz 6 imion - pytając się za każdym razem użytkownika 
+# W nowej tabeli wpisz ile ma znaków każde z imion 
+
+#   Grupa 2
+# Do tabeli wpisz 6 owoców - pytając się za każdym razem użytkownika o jego ulubiony owoc
+# W nowej tabeli wpisz ile ma znaków każdy z owoców
