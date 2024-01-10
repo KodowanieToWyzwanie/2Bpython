57ad5fb1c77656028e50f8e856d3b3db8beab431
KodowanieToWyzwanie <148467446+KodowanieToWyzwanie@users.noreply.github.com>
2024-01-10 21:02:55 +0100

    Update zestaw1odpowiedzi.py
    contains solutions for tasks for mirror numbers

--- Sprawdziany/zestaw1odpowiedzi.py
+++ Sprawdziany/zestaw1odpowiedzi.py
@@ -34,32 +34,6 @@
 # else:
 #   print("bruh")
 
-#Zadanie 3
-# W liczbie trzycyfrowej została zatarta cyfra dziesiątek: 3*4. Jaka jest cyfra dziesiątek jeżeli wiadomo, że liczba ta jest podzielna przez 6 ale nie jest podzielna przez 9?
-
-# for i in range(0, 10):
-
-#   liczba = 304 + (10 * i)
-#   if liczba % 6 == 0 and liczba % 9 != 0:
-#     print(liczba)
-
-#zadanie 4
-#Dwucyfrowa liczba została podzielona przez sumę swoich cyfr. oblicz resztę tego działania? Sprawdź dla kilku przypadków
-
-# def najwieksza_reszta(liczba):
-#   suma = (liczba % 10 ) * 10 + liczba //10
-#   return liczba%suma
-# print(najwieksza_reszta(54))
-
-
-# #Zadanie 5 
-# # Czy z podanych trzech boków można utworzyć trójkąt?
-
-a = int(input())
-b = int(input())
-c = int(input())
-
-
 
 #Liczba lustrzana
 # def lbstr(a):
@@ -71,44 +45,61 @@ c = int(input())
 #   for a in L:
 #     x += a
 #   return int(x)
+
 # Zadanie 1
 # Oblicz sumę dowolnej liczby dwucyfrowej i liczby lustrzanej. Podaj dwa największe dzielniki otrzymanej liczby. Liczbę lustrzaną otrzymujemy z danej liczby przez przestawienie jej cyfr.
-# def lbstr(a):
-#   lustrzana = (a % 10)*10 + a//10
-#   return lustrzana
-# def sum(a):
-#   if a < 100:
-#     al = lbstr(a)
-#     sum = a+al
-#     idx = 0
-#     print(sum)
-#     for i in range(sum, 1, -1):
-#       if idx < 2 and sum%i == 0:
-#         print(sum//i, i)
-#         idx += 1
-# sum(32)    
+def lbstr(a):
+  lustrzana = (a % 10)*10 + a//10
+  return lustrzana
+def sum(a):
+  if a < 100:
+    al = lbstr(a)
+    sum = a+al
+    idx = 1
+    print(f"{a} i lustrzana {al} ich suma {sum}")
+    for i in range(sum, 1, -1):
+      if idx <= 2 and sum%i == 0:
+        print(f"{idx} dzielnik: {i}")
+        idx += 1
+sum(45)
+sum(32)   
 
 
 # Zadanie 2
 # Oblicz sumę dowolnej liczby dwucyfrowej i liczby lustrzanej.  Liczbę lustrzaną otrzymujemy z danej liczby przez przestawienie jej cyfr. sprawdz czy dla kliku wybranych liczb otrzymana suma dzieli się przez sumę cyfr jednej z liczb dwucyfrowej - tj. czy np. 42 +24 dzieli się przez 6
 
+def lbstr(a):
+  lustrzana = (a % 10)*10 + a//10
+  return lustrzana
+def sum(a):
+  sum = 0
+  if a < 100:
+    al = lbstr(a)
+    sum = a+al
+  return sum
+def suma_cyfr(a):
+  return a//10+a%10 
+def czy_dzieli(a):
+  return sum(a)%suma_cyfr(a) ==0
 
+print(czy_dzieli(45))
+print(czy_dzieli(89))
 
 
 
 #Zadanie 3
 # W liczbie trzycyfrowej została zatarta cyfra dziesiątek: 3*4. Jaka jest cyfra dziesiątek jeżeli wiadomo, że liczba ta jest podzielna przez 6 ale nie jest podzielna przez 9?
-# for i in range(10):
-#   liczba = (304 + (i*10))
-#   if liczba % 6 == 0 and liczba % 9 != 0:
-#     print(liczba)  
+for i in range(10):
+  liczba = (304 + (i*10))
+  if liczba % 6 == 0 and liczba % 9 != 0:
+    print(liczba)  
 
 #zadanie 4
 #Dwucyfrowa liczba została podzielona przez sumę swoich cyfr. oblicz resztę tego działania? Sprawdź dla kilku przypadków
-# suma=0
-# x=int(input())
-# suma=(x % 10 + x//10)
-# print(x%suma)
+suma=0
+x=int(input())
+suma=(x % 10 + x//10)
+print(x%suma)
 
 
 
@@ -116,10 +107,10 @@ c = int(input())
 # # Czy z podanych trzech boków można utworzyć trójkąt?
 
 
-# a = int(input())
-# b = int(input())
-# c = int(input())
-# if (a+b > c) and (a+c > c) and (b+c > a):
-#   print("tak")
-# else:
-#   print("nie")
+a = int(input())
+b = int(input())
+c = int(input())
+if (a+b > c) and (a+c > c) and (b+c > a):
+  print("tak")
+else:
+  print("nie")
