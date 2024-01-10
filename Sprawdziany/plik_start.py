8cfedbedbda3f52cd4f78fe365699b4f972c35c7
KodowanieToWyzwanie <148467446+KodowanieToWyzwanie@users.noreply.github.com>
2024-01-10 14:16:43 +0100

    Update plik_start.py
    Rozwiązanie

--- Sprawdziany/plik_start.py
+++ Sprawdziany/plik_start.py
@@ -5,3 +5,13 @@
 #   Grupa 2
 # Do tabeli wpisz 6 owoców - pytając się za każdym razem użytkownika o jego ulubiony owoc
 # W nowej tabeli wpisz ile ma znaków każdy z owoców
+
+
+owoce = []
+dl = []
+for i in range(6):
+  owoce.append(input("Podaj owoc\n"))
+  dl.append(len(owoce[i]))
+
+print(owoce)
+print(dl)
