ad55e32b02977eceae00d69cf29fa2ddbea4d9c3
KodowanieToWyzwanie <148467446+KodowanieToWyzwanie@users.noreply.github.com>
2024-01-10 20:35:24 +0100

    Update liczby_temp_1kolumna.py
    be used to solve basic problems with files with numbers in python

---
+++ ZadaniaLekcyjne/praca_z_plikami/liczby_temp_1kolumna.py
@@ -0,0 +1,7 @@
+#zad 1. ile jest temperatur poniżej 0, wyświetl wynik dla każdego zadania
+#zad 2. jaka jest najniższa temperatura - bez używania funkcji wbudowanej
+#zad 3. jaka jest średnia temperatura - zaokrąglij do 2 miejsc po przecinku
+#zad 4. ile jest liczb całkowitych
+#zad 5. sprawdż ile w pliku jest liczb pierwszych - utwórz dodatkowo nową tabelą z liczbami pierwszymi. Wyświetl tablicę z liczbami pierszymi i liczbę znalezionych liczb pierwszych 
+#zad 6. ile jest liczb, które są kwadratem liczby. np. 4 jest kwadratem liczby 2, 1 jest kwadratem 1. utwórz dodatkowo nową tabelą z tymi liczbami i oblicz ile ich jest
+#zad 6. ile jest liczb, które są sześcianem liczby - Sześcian liczby to wynik przemnożenia trzy razy liczby przez siebie . np. 64 sześcian liczby 4. utwórz dodatkowo nową tabelą z tymi liczbami i oblicz ile ich jest
