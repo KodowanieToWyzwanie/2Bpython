d2a5889300423a8b08beca94aa0544c2dc502d52
KodowanieToWyzwanie <148467446+KodowanieToWyzwanie@users.noreply.github.com>
2024-01-12 12:18:11 +0100

    Add files via upload
    help to repeat all the information, and basic programming concept

---
+++ ZadaniaLekcyjne/minimaln.py
@@ -0,0 +1,14 @@
+# Minimalna
+# Limit pamięci: 64 MB
+# W długie zimowe wieczory Antek z Zuzią lubią bawić się w pisanie liczb na małych różnokolorowych karteczkach. Na każdej z karteczek zapisują tylko jedną liczbę. Na zakończenie zabawy mieszają wszystkie karteczki i losują trzy z nich. Wygrywa ta karteczka z wylosowanych, na której napisana jest minimalna liczba.
+# Zadanie
+# Napisz program, który:
+# wczyta ze standardowego wejścia trzy liczby całkowite,znajdzie minimalną z nich,
+# wypisze wynik na standardowe wyjście.
+# WejścieW trzech kolejnych wierszach zapisane są liczby całkowite z zakresu od  do .
+# Wyjście Program powinien wypisać minimalną liczbę z podanych na wejściu.
+# Przykład
+# Dla danych wejściowych:
+# 19 18 1973241
+# poprawną odpowiedzią jest:
+# 18
