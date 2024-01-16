7859f2dedb3e77a2840fdebdd63c8fef8cd9be96
KodowanieToWyzwanie <148467446+KodowanieToWyzwanie@users.noreply.github.com>
2024-01-16 21:25:46 +0100

    Create kartkowka_plik_17_01_2024.py

---
+++ Sprawdziany/kartkowka_plik_17_01_2024.py
@@ -0,0 +1,12 @@
+# Utwórz plik auta.txt z danymi poniżej. Wczytaj plik. Znajdź wśród podanych aut, te które ma najwięcej znaków - podaj tą nazwę i liczbę znaków.
+
+Corolla
+F
+Golf
+Civic
+RAV4
+CR-V
+Tiguan
+Focus
+Polo
+Camry
