3b8d3e80abddf3ab28235a3427e13b9bf7fd6c55
KodowanieToWyzwanie <148467446+KodowanieToWyzwanie@users.noreply.github.com>
2024-01-19 12:04:15 +0100

    Create algorytmy_listy.py
    help to do the basic algoritm on lists

---
+++ NotatkiLekcja/algorytmy_listy.py
@@ -0,0 +1,41 @@
+
+# x = 0
+# x = "p"
+# print(x)
+# del x
+# print(x)
+# tab = [4, 5, 6, 2, 3, 1]
+# n = len(tab)
+# #sortowanie
+# for j in range(n):
+#   for i in range(n - 1 - j):
+#     if tab[i] > tab[i + 1]:
+#       tab[i], tab[i + 1] = tab[i + 1], tab[i]
+
+# print(tab)
+# #min/max
+# mini = tab[0]
+# for i in range(n):
+#   if mini > tab[i]:
+#     mini = tab[i]
+# #suma elementow   
+# suma = 0
+# for i in range(n):
+#   suma += tab[i]
+# print(suma)
+# print(sum(tab))
+# print(mini)
+# print(min(tab))
+
+# filepath = "owoc.txt"
+# f = open(filepath,"r")
+# lines = f.readlines()
+
+# print(lines)
+# # for line in f:
+# #   print(line)
+# print(f.read(10))
+# print(f.read(8))
+# print(f.closed)
+# f.close()
+# print(f.closed)
