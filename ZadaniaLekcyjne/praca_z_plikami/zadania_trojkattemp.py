7209be0b14ce38771ddfd916ef1367265272b28d
KodowanieToWyzwanie <148467446+KodowanieToWyzwanie@users.noreply.github.com>
2024-01-30 20:05:01 +0100

    Create zadania_trojkattemp.py

---
+++ ZadaniaLekcyjne/praca_z_plikami/zadania_trojkattemp.py
@@ -0,0 +1,6 @@
+1. Wczytaj o użytkownika ile lubi owoców, pobieraj je w pętli i zapisz do pliku 
+2. wczytaj plik trojkat.txt w ilu wierszach zaprezentowane są dane, z których można zbudować trójkąt?
+3. wczytaj dane z pliku anagramy.txt, a następnie oblicz w ilu linijkach są anagramy - 
+słowa, w których są te same litery w innej kolejności, na ekranie wyświetl linijki z anagramami oraz zsumuj ich liczbę i wyświetl. 
+4. w pliku temperatury.txt są dane pomiarowe z kilkunastu dni. 
+Sprawdź przy pomocy programu komputerowego, dla ilu dni wynik jest powyżej i równy zero, a dla ilu poniżej. Wyświetl na ekranie
